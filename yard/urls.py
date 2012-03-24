1bc86351be655776f50100bb7e16b3900f6f1519
Diogo Laginha <dmachado@ipn.pt>
2012-03-24 17:04:55 +0000

    change yard.urls include function name

--- yard/urls.py
+++ yard/urls.py
@@ -1,23 +1,23 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-from django.conf.urls import defaults
+from django.conf.urls.defaults import patterns, url, include
 
 # allowed http-methods mapped with respective resource-method
 collection = {'get':'index', 'post':'create'}
 single     = {'get':'show', 'put':'update', 'post':'update', 'delete':'destroy'}
 
-def include(resource):
+def include_resource(resource):
     '''
     include resource urls for single and collection access
     '''
-    urlpatterns = defaults.patterns('',
+    urlpatterns = patterns('',
         # url for resource collection access
-        defaults.url( r'^/(?P<id>[0-9]+)/$', resource(single) ),
+        url( r'^/(?P<id>[0-9]+)/$', resource(single) ),
         # url for single resource access
-        defaults.url( r'^/$',                resource(collection) ),
+        url( r'^/$',                resource(collection) ),
     )
-    return defaults.include( urlpatterns )
+    return include( urlpatterns )
 
 
 
