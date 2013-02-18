f5c83f4b55043211f631bd552a345286778118c8
Diogo Laginha <dmachado@ipn.pt>
2013-02-18 18:05:35 +0000

    look for version name in http accept

--- src/yard/resources/__init__.py
+++ src/yard/resources/__init__.py
@@ -30,7 +30,7 @@ class Resource(object):
         pass
 
     def __init__(self, api, routes):
-        self.__api        = api
+        self._api         = api
         self.__routes     = routes # maps http methods with respective views
         self.__meta       = ResourceMeta( self.Meta )
         self.__pagination = ResourcePage( self.Pagination )
@@ -106,8 +106,8 @@ class Resource(object):
     def __get_builder(self, fields, parameters):
         if callable(fields):
             current_fields = fields(parameters)
-            return JSONbuilder( self.__api, current_fields ), current_fields
-        return JSONbuilder( self.__api, fields ), fields
+            return JSONbuilder( self._api, current_fields ), current_fields
+        return JSONbuilder( self._api, fields ), fields
 
     def __view(self, request, method, parameters):
         '''
@@ -208,7 +208,7 @@ class Resource(object):
         return [builder.to_json(i) for i in resources]
 
     def serialize_all(self, resources, fields):
-        builder = JSONbuilder( self.__api, fields )
+        builder = JSONbuilder( self._api, fields )
         return self.__serialize_all( resources, builder )
 
     def __serialize(self, resource, builder):
@@ -218,6 +218,6 @@ class Resource(object):
         return builder.to_json(resource)
 
     def serialize(self, resource, fields):
-        builder = JSONbuilder( self.__api, fields )
+        builder = JSONbuilder( self._api, fields )
         return self.__serialize( resource, builder )
 
