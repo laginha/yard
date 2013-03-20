355a6a171d1880f1a6a023d21598da0b229bed5c
Rui Chicoria <rchicoria@ipn.pt>
2013-03-20 17:02:38 +0000

    Added support for Generic Relations.

--- src/yard/utils/__init__.py
+++ src/yard/utils/__init__.py
@@ -10,80 +10,27 @@ import inspect
 
 float_  = re.compile(r'^\-?[0-9]+\.[0-9]+$')
 
-
-def is_serializable(x):
-    return isinstance(x, (int,str,unicode,float,list,dict, NoneType))
-    
-def is_float(x):
-    return isinstance(x, float)
-    
-def is_int(x):
-    return isinstance(x, int)
-    
-def is_str(x):
-    return isinstance(x, str)
-    
-def is_unicode(x):
-    return isinstance(x, unicode)
-    
-def is_tuple(x):
-    return isinstance(x, tuple)
-    
-def is_dict(x):
-    return isinstance(x, dict)
-    
-def is_list(x):
-    return isinstance(x, list)
-    
-def is_file(x):
-    return isinstance(x, file)
-    
-def is_iter(x):
-    return hasattr(x, '__iter__')
-    
-def is_strfloat(x):
-    return bool( float_.match(x) ) if x else False
-    
-def is_strint(x): 
-    return x.isdigit() if isinstance(x, (unicode,str)) else False
-    
-def is_method(x):
-    return inspect.ismethod(x)
-    
-def is_queryset(x):
-    return isinstance(x, QuerySet)
-    
-def is_valuesset(x):
-    return isinstance(x, ValuesQuerySet)
-    
-def is_modelinstance(x):
-    return isinstance(x, models.Model)
-    
-def is_httpresponse(x):
-    return isinstance(x, HttpResponse)
-    
-def is_generator(x):
-    return isinstance(x, GeneratorType)
-    
-def is_many_related_manager(x): 
-    name = "django.db.models.fields.related.ManyRelatedManager"
-    return name in str(type(x))
-    
-def is_related_manager_object(x):
-    name = "django.db.models.fields.related.RelatedManager"
-    return name in str(type(x))
-    
-def is_generic_related_manager(x):
-    name = "django.contrib.contenttypes.generic.GenericRelatedObjectManager"
-    return name in str(type(x))
-    
-def is_related_manager(x): 
-    functions = [
-        is_many_related_manager, 
-        is_related_manager_object, 
-        is_generic_related_manager,
-    ]
-    for each in functions:
-        if each(x):
-            return True
-    return False
+current_url       = lambda x: "%s/%s" %(x.get_host(), x.META.get('PATH_INFO', ''))
+is_serializable   = lambda x: isinstance(x, (int,str,unicode,float,list,dict, NoneType))
+is_float          = lambda x: isinstance(x, float)
+is_int            = lambda x: isinstance(x, int)
+is_str            = lambda x: isinstance(x, str)
+is_unicode        = lambda x: isinstance(x, unicode)
+is_tuple          = lambda x: isinstance(x, tuple)
+is_dict           = lambda x: isinstance(x, dict)
+is_list           = lambda x: isinstance(x, list)
+is_file           = lambda x: isinstance(x, file)
+is_iter           = lambda x: hasattr(x, '__iter__')
+is_strfloat       = lambda x: bool( float_.match(x) ) if x else False
+is_strint         = lambda x: x.isdigit() if isinstance(x, (unicode,str)) else False
+is_method         = lambda x: inspect.ismethod(x)
+is_geo_value      = lambda x: is_geodj_project and isinstance(x, GEOSGeometry)
+is_geo            = lambda x: is_geodj_project and isinstance(x, GeometryField)
+is_queryset       = lambda x: isinstance(x, QuerySet)
+is_valuesset      = lambda x: isinstance(x, ValuesQuerySet)
+is_modelinstance  = lambda x: isinstance(x, models.Model)
+is_httpresponse   = lambda x: isinstance(x, HttpResponse)
+is_generator      = lambda x: isinstance(x, GeneratorType)
+is_manyrelatedmanager = lambda x: "django.db.models.fields.related.ManyRelatedManager" in str(type(x))
+is_relatedmanager     = lambda x: "django.db.models.fields.related.RelatedManager" in str(type(x))
+is_genericrelatedobjectmanager     = lambda x: "django.contrib.contenttypes.generic.GenericRelatedObjectManager" in str(type(x))
