1cb123934c207b587529f25e6f7334bac4935b9b
Diogo Laginha <dmachado@ipn.pt>
2013-04-25 00:43:14 +0100

    code changes

--- src/yard/apps/keyauth/__init__.py
+++ src/yard/apps/keyauth/__init__.py
@@ -1,6 +1,33 @@
 #!/usr/bin/env python
 # encoding: utf-8
+from django.contrib.auth import authenticate
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.conf import settings
 from yard.apps.keyauth.models import Consumer
+
+KEY_PARAMETER_NAME = getattr(settings, 'KEY_PARAMETER_NAME', 'key')
+KEY_AUTH_401_TEMPLATE = getattr(settings, 'KEY_AUTH_401_TEMPLATE', None)
+
+if KEY_AUTH_401_TEMPLATE:
+    HttpResponse401 = lambda r: render(r, **KEY_AUTH_401_TEMPLATE)
+else:
+    KEY_AUTH_401_CONTENT = getattr(settings, 'KEY_AUTH_401_CONTENT', "")
+    HttpResponse401 = lambda r: HttpResponse(KEY_AUTH_401_CONTENT, status=401)
+
+
+def key_required_wrapper(f):
+    '''
+    Auxiliar decorator for keyauth decorator and middleware
+    '''
+    def wrapper(klass, request, *args, **kwargs):
+        key = authenticate(token=request.REQUEST.get(KEY_PARAMETER_NAME))
+        if key and is_valid_consumer(request, key):
+            key.save()
+            return f(klass, request, *args, **kwargs)
+        return HttpResponse401( request )
+    return wrapper
+
 
 def is_valid_consumer(request, key):
     '''
@@ -12,3 +39,5 @@ def is_valid_consumer(request, key):
     except Consumer.DoesNotExist:
         consumers = Consumer.objects.filter(key=key, allowed=True)
         return not consumers.exists()
+
+
