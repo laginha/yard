fc8d0c3d70eaffeff070a8cbf83eca6a7a579362
Diogo Laginha <dmachado@ipn.pt>
2013-05-21 14:48:10 +0100

    update version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name             = 'yard',
-    version          = '1.5.0',
+    version          = '2.0.0',
     author           = "Diogo Laginha",
     url              = 'https://github.com/laginha/yard',
     description      = "Yet Another Resftul Django-app",
