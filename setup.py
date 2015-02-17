0f78b862509dadbf10210c0dd84db98f982c0637
Diogo Machado <diogo.laginha.machado@gmail.com>
2015-02-17 10:53:17 +0000

    fix requirements

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@
 from setuptools import setup, find_packages
 import pip
 
-def parse_requirements():
-    return pip.req.parse_requirements('requirements.txt')
-    
-install_requires = [
-    str(each.req) for each in parse_requirements() if each.req
-]
+# def parse_requirements():
+#     return pip.req.parse_requirements('requirements.txt')
+#
+# install_requires = [
+#     str(each.req) for each in parse_requirements() if each.req
+# ]
 
 setup(
     name             = 'yard',
@@ -18,7 +18,7 @@ setup(
     description      = "Yet Another Resftul Django-app",
     packages         = find_packages(where='src'),
     package_dir      = {'': 'src'},
-    install_requires = install_requires,
+    install_requires = ['Django', 'simplejson', 'rstr', 'gpolyencode'],
     # dependency_links = ,
     extras_require   = {},
     zip_safe         = False,
