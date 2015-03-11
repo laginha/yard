85787c1419686cd96c1674c85ac66af2b56f83a8
Diogo Machado <diogo.laginha.machado@gmail.com>
2015-03-11 16:27:06 +0000

    fix setup file

--- setup.py
+++ setup.py
@@ -1,25 +1,27 @@
 #!/usr/bin/env python
 # encoding: utf-8
 from setuptools import setup, find_packages
-import pip
+from pip.req import parse_requirements
 
-# def parse_requirements():
-#     return pip.req.parse_requirements('requirements.txt')
-#
-# install_requires = [
-#     str(each.req) for each in parse_requirements() if each.req
-# ]
+install_reqs = parse_requirements("requirements.txt")
+reqs = [str(each.req) for each in install_reqs if each.req]
 
 setup(
     name             = 'yard',
     version          = '2.2.0',
     author           = "Diogo Laginha",
+    author_email     = "diogo.laginha.machado@gmail.com",
     url              = 'https://github.com/laginha/yard',
-    description      = "Yet Another Resftul Django-app",
+    description      = "Yet Another Resftul Django framework",
     packages         = find_packages(where='src'),
     package_dir      = {'': 'src'},
-    install_requires = ['Django', 'simplejson', 'rstr', 'gpolyencode'],
-    # dependency_links = ,
+    install_requires = reqs,
     extras_require   = {},
     zip_safe         = False,
+    license          = 'MIT',
+    classifiers      = [
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Intended Audience :: Developers',
+    ]
 )
